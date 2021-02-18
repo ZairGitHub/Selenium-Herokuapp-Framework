620920dff0b429f6751c597d05658cfc6479746e
Zair <michaelxzr@gmail.com>
2021-02-18 20:51:14 +0000

    Create ConfigReader.cs

---
+++ SeleniumExamples/SeleniumExamples/ConfigReader.cs
@@ -0,0 +1,19 @@
+﻿using System.Configuration;
+
+namespace SeleniumExamples
+{
+    public static class ConfigReader
+    {
+        public static readonly string BaseUrl =
+            ConfigurationManager.AppSettings["base_url"];
+
+        public static readonly string AddRemoveElements =
+            ConfigurationManager.AppSettings["add_remove_elements"];
+
+        public static readonly string BasicAuthentication =
+            ConfigurationManager.AppSettings["basic_authentication"];
+
+        public static readonly string FormAuthetication =
+            ConfigurationManager.AppSettings["form_authentication"];
+    }
+}

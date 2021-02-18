de6ec5ae59e601c752214b31b6a5b479c321cd50
Zair <michaelxzr@gmail.com>
2021-02-18 21:15:14 +0000

    Rename AddRemove.cs to AddRemovePage.cs
    Update AddRemovePage.cs.

---
+++ SeleniumExamples/SeleniumExamples/Pages/AddRemovePage.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.ObjectModel;
+using OpenQA.Selenium;
+
+namespace SeleniumExamples.Pages
+{
+    public class AddRemovePage
+    {
+        private readonly string _url = ConfigReader.BaseUrl + ConfigReader.AddRemoveElements;
+        private readonly IWebDriver _driver;
+
+        public AddRemovePage(IWebDriver driver) => _driver = driver;
+
+        public IWebElement ButtonAdd =>
+            _driver.FindElement(By.CssSelector("button"));
+
+        public IWebElement ButtonDelete =>
+            _driver.FindElement(By.CssSelector(".added-manually"));
+            
+        public ReadOnlyCollection<IWebElement> ButtonsDelete() =>
+            _driver.FindElements(By.CssSelector(".added-manually"));
+        
+        private void NavigateToPage() => _driver.Navigate().GoToUrl(_url);
+    }
+}

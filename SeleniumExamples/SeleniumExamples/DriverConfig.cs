eca3913931ae98d1da2271dac96b76bbb1bbfe47
Zair <michaelxzr@gmail.com>
2021-02-18 21:04:27 +0000

    Update DriverConfig.cs

--- SeleniumExamples/SeleniumExamples/DriverConfig.cs
+++ SeleniumExamples/SeleniumExamples/DriverConfig.cs
@@ -3,7 +3,23 @@ using OpenQA.Selenium;
 
 namespace SeleniumExamples
 {
-    public class DriverConfig
+    public class DriverConfig<T> where T : IWebDriver, new()
     {
+        public DriverConfig(int elementWaitTime, int pageWaitTime)
+        {
+            Driver = new T();
+            ConfigureDriver(elementWaitTime, pageWaitTime);
+        }
+
+        public T Driver { get; private set; }
+
+        private void ConfigureDriver(int elementWaitTime, int pageWaitTime)
+        {
+            Driver.Manage().Timeouts().ImplicitWait = 
+                TimeSpan.FromSeconds(elementWaitTime);
+
+            Driver.Manage().Timeouts().PageLoad =
+                TimeSpan.FromSeconds(pageWaitTime);
+        }
     }
 }

69235ae440e0bd38f16d9b47162e8a4ab8d4cd12
Zair <michaelxzr@gmail.com>
2021-02-19 11:17:48 +0000

    Update FormAuthenticationPage.cs

--- SeleniumExamples/SeleniumExamples/Pages/FormAuthenticationPage.cs
+++ SeleniumExamples/SeleniumExamples/Pages/FormAuthenticationPage.cs
@@ -9,8 +9,22 @@ namespace SeleniumExamples.Pages
 
         public FormAuthenticationPage(IWebDriver driver) => _driver = driver;
 
+        public IWebElement FieldUsername => _driver.FindElement(By.Id("username"));
+        
+        public IWebElement FieldPassword => _driver.FindElement(By.Id("password"));
+        
+        public IWebElement ButtonLogin => _driver.FindElement(By.CssSelector(".fa"));
+        
+        public IWebElement ButtonLogOut => _driver.FindElement(By.CssSelector(".icon-2x"));
+
         public void NavigateToPage() => _driver.Navigate().GoToUrl("http://the-internet.herokuapp.com/login");
 
         public void CloseDriver() => _driver.Quit();
+
+        public void EnterUsername(string input) => FieldUsername.SendKeys(input);
+
+        public void EnterPassword(string input) => FieldPassword.SendKeys(input);
+
+        public void ClickLoginButton() => ButtonLogin.Click();
     }
 }

7a0751a7ca8b70792445795ddded524224228711
Zair <michaelxzr@gmail.com>
2021-02-21 18:49:33 +0000

    Create DigestAuthenticationPage.cs

---
+++ SeleniumExamples/SeleniumExamples/Pages/DigestAuthenticationPage.cs
@@ -0,0 +1,9 @@
+﻿namespace SeleniumExamples.Pages
+{
+    public static class DigestAuthenticationPage
+    {
+        public static string ValidUsername => "admin";
+
+        public static string ValidPassword => "admin";
+    }
+}

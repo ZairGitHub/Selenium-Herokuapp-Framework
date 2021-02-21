06469edf3a2277eb044246722164ab09287fc3a5
Zair <michaelxzr@gmail.com>
2021-02-21 11:01:30 +0000

    Update BasicAuthenticationPage.cs

--- SeleniumExamples/SeleniumExamples/Pages/BasicAuthenticationPage.cs
+++ SeleniumExamples/SeleniumExamples/Pages/BasicAuthenticationPage.cs
@@ -4,5 +4,20 @@ namespace SeleniumExamples.Pages
 {
     public class BasicAuthenticationPage
     {
+        private readonly IWebDriver _driver;
+
+        public BasicAuthenticationPage(IWebDriver driver) => _driver = driver;
+
+        public string ValidUsername => "admin";
+
+        public string ValidPassword => "admin";
+
+        private IAlert AlertBasicAuthentication => _driver.SwitchTo().Alert();
+
+        public void ClickOKButton() => AlertBasicAuthentication.Accept();
+
+        public void ClickCancelButton() => AlertBasicAuthentication.Dismiss();
+
+        public void DeleteAllCookies() => _driver.Manage().Cookies.DeleteAllCookies();
     }
 }

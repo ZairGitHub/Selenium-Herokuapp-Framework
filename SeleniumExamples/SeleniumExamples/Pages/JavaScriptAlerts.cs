ab84296a6560308b4b7b348999c7b0a2351a6bb5
Zair <michaelxzr@gmail.com>
2021-02-21 21:19:46 +0000

    Update JavaScriptAlerts.cs

--- SeleniumExamples/SeleniumExamples/Pages/JavaScriptAlerts.cs
+++ SeleniumExamples/SeleniumExamples/Pages/JavaScriptAlerts.cs
@@ -4,5 +4,27 @@ namespace SeleniumExamples.Pages
 {
     public class JavaScriptAlerts
     {
+        private readonly IWebDriver _driver;
+
+        public JavaScriptAlerts(IWebDriver driver) => _driver = driver;
+
+        private IWebElement ButtonJSAlert =>
+            _driver.FindElement(By.CssSelector("li:nth-child(1) > button"));
+
+        private IWebElement ButtonJSConfirm =>
+            _driver.FindElement(By.CssSelector("li:nth-child(2) > button"));
+
+        private IWebElement ButtonJSPrompt =>
+            _driver.FindElement(By.CssSelector("li:nth-child(3) > button"));
+
+        private IWebElement TextResult => _driver.FindElement(By.Id("result"));
+
+        public void ClickJSAlertButton() => ButtonJSAlert.Click();
+
+        public void ClickJSConfirmButton() => ButtonJSConfirm.Click();
+
+        public void ClickJSPromptButton() => ButtonJSPrompt.Click();
+
+        public string ReadResultText() => TextResult.Text;
     }
 }

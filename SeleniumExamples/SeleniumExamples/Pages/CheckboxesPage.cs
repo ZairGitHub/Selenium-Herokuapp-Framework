eba55c1646f89d0547183d658ed6e314ea8ef929
Zair <michaelxzr@gmail.com>
2021-02-21 23:43:57 +0000

    Create CheckboxesPage.cs

---
+++ SeleniumExamples/SeleniumExamples/Pages/CheckboxesPage.cs
@@ -0,0 +1,11 @@
+﻿using OpenQA.Selenium;
+
+namespace SeleniumExamples.Pages
+{
+    public class CheckboxesPage
+    {
+        private IWebDriver _driver;
+
+        public CheckboxesPage(IWebDriver driver) => _driver = driver;
+    }
+}

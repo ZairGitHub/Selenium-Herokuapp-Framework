7f4b5e6fa32957465cad1155f01a5f2606cee793
Zair <michaelxzr@gmail.com>
2021-02-21 23:49:45 +0000

    Create CheckboxesTests.cs

---
+++ SeleniumExamples/SeleniumExamples/Tests/CheckboxesTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using SeleniumExamples.Pages;
+
+namespace SeleniumExamples
+{
+    [TestFixture]
+    public class CheckboxesTests
+    {
+        private WebsitePOM _sut;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp() => _sut = new WebsitePOM();
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown() => _sut.CloseDriver();
+    }
+}

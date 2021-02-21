e020ca3c66dd2a7d24fef3c2ee3101b5dbd7507e
Zair <michaelxzr@gmail.com>
2021-02-21 23:49:09 +0000

    Update CheckboxesPage.cs

--- SeleniumExamples/SeleniumExamples/Pages/CheckboxesPage.cs
+++ SeleniumExamples/SeleniumExamples/Pages/CheckboxesPage.cs
@@ -7,5 +7,15 @@ namespace SeleniumExamples.Pages
         private IWebDriver _driver;
 
         public CheckboxesPage(IWebDriver driver) => _driver = driver;
+
+        private IWebElement Checkbox1 =>
+            _driver.FindElement(By.CssSelector("input:nth-child(1)"));
+
+        private IWebElement Checkbox2 =>
+            _driver.FindElement(By.CssSelector("input:nth-child(3)"));
+
+        public void ClickCheckbox1() => Checkbox1.Click();
+
+        public void ClickCheckbox2() => Checkbox2.Click();
     }
 }

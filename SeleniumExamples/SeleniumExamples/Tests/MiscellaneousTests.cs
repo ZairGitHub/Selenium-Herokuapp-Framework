c531b818bcb76fd37af8f86ab0a19eeb870c4d24
Zair <michaelxzr@gmail.com>
2021-02-22 23:27:47 +0000

    Add [OneTimeSetUp] and [OneTimeTearDown] to MiscellaneousTests.cs

--- SeleniumExamples/SeleniumExamples/Tests/MiscellaneousTests.cs
+++ SeleniumExamples/SeleniumExamples/Tests/MiscellaneousTests.cs
@@ -7,33 +7,32 @@ namespace SeleniumExamples
     {
         private WebsitePOM _sut;
 
-        private WebsitePOM CreateDefaultWebsitePOM() => new WebsitePOM();
+        [OneTimeSetUp]
+        public void OneTimeSetUp() => _sut = new WebsitePOM();
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown() => _sut.CloseDriver();
 
         [Test]
         public void InvalidUrl_RedirectsToEmptyPageWithNotFoundError()
         {
-            _sut = CreateDefaultWebsitePOM();
-
             _sut.NavigateToInvalidPage();
+
             var result = _sut.SharedHTML.ReadPageHeaderText();
 
             Assert.That(result, Is.EqualTo("Not Found"));
-
-            _sut.CloseDriver();
         }
 
         [Test]
         public void AuthenticationPopup_OKButton_CreatesNewAuthenticationPopup()
         {
-            _sut = CreateDefaultWebsitePOM();
-
             _sut.NavigateToBasicAuthenticationPage();
+
             _sut.SharedIAlert.ClickOKButton();
             var result = _sut.SharedIAlert.AuthenticationPopupExists();
+            _sut.SharedIAlert.ClickCancelButton();
 
             Assert.That(result, Is.True);
-
-            _sut.CloseDriver();
         }
     }
 }

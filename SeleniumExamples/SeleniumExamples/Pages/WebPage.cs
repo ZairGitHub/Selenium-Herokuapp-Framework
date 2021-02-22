d523695ca89e488e37e52298e7a670672f983c22
Zair <michaelxzr@gmail.com>
2021-02-22 15:19:44 +0000

    Create abstract WebPage.cs

---
+++ SeleniumExamples/SeleniumExamples/Pages/WebPage.cs
@@ -0,0 +1,11 @@
+﻿using OpenQA.Selenium;
+
+namespace SeleniumExamples.Pages
+{
+    public abstract class WebPage
+    {
+        protected IWebDriver Driver { get; }
+
+        public WebPage(IWebDriver driver) => Driver = driver;
+    }
+}

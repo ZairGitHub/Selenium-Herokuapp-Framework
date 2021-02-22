e0fb120416470baf75d81bee0096f259e0dd34f5
Zair <michaelxzr@gmail.com>
2021-02-22 18:13:25 +0000

    Create HoversTests.cs

---
+++ SeleniumExamples/SeleniumExamples/Tests/HoversTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using SeleniumExamples.Pages;
+
+namespace SeleniumExamples
+{
+    [TestFixture]
+    public class HoversTests
+    {
+        private WebsitePOM _sut;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp() => _sut = new WebsitePOM();
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown() => _sut.CloseDriver();
+    }
+}

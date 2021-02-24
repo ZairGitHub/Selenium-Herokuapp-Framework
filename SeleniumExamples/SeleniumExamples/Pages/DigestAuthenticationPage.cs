460cd3124464bdd8c157533fd57ba42ae258b022
Zair <michaelxzr@gmail.com>
2021-02-24 15:56:56 +0000

    Implement IAlertNavigation in DigestAuthenticationPage.cs
    Remove static from class.
    Add WebPage.cs inheritance to class.

--- SeleniumExamples/SeleniumExamples/Pages/DigestAuthenticationPage.cs
+++ SeleniumExamples/SeleniumExamples/Pages/DigestAuthenticationPage.cs
@@ -1,9 +1,25 @@
-﻿namespace SeleniumExamples.Pages
-{
-    public static class DigestAuthenticationPage
-    {
-        public static string ValidUsername => "admin";
+﻿using OpenQA.Selenium;
 
-        public static string ValidPassword => "admin";
+namespace SeleniumExamples.Pages
+{
+    public class DigestAuthenticationPage : WebPage, IAlertNavigation
+    {
+        private const string _validUsername = "admin";
+        private const string _validPassword = "admin";
+
+        public DigestAuthenticationPage(IWebDriver driver) : base(driver) { }
+
+        public void NavigateToAuthentication(
+            string username = null, string password = null)
+        {
+            //NavigateToURL(ConfigReader.Index + ConfigReader.DigestAuthentication);
+            NavigateToURL(
+                $"http://{username}:{password}@" +
+                $"the-internet.herokuapp.com/digest_auth");
+        }
+
+        public string ValidUsername => "admin";
+
+        public string ValidPassword => "admin";
     }
 }

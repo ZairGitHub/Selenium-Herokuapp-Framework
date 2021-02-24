1ee99e520694169e6e986bc5575efec11fd9efec
Zair <michaelxzr@gmail.com>
2021-02-24 19:59:52 +0000

    Create StaticDriver.cs

---
+++ SeleniumExamples/SeleniumExamples/StaticDriver.cs
@@ -0,0 +1,13 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+
+namespace SeleniumExamples
+{
+    public static class StaticDriver
+    {
+        //public static IWebDriver Driver = new ChromeDriver();
+
+        public static IWebDriver Driver = new FirefoxDriver();
+    }
+}

c021704a87495f117d36f8770ac0d271ea067903
Zair <michaelxzr@gmail.com>
2021-02-25 21:32:58 +0000

    Create IndexSteps.cs

---
+++ SeleniumExamples/SeleniumExamples/Steps/IndexSteps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace SeleniumExamples.Steps
+{
+    [Binding]
+    public class IndexSteps
+    {
+        [Given(@"I am on the Index page")]
+        public void GivenIAmOnTheIndexPage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I click the ""(.*)"" link")]
+        public void WhenIClickTheLink(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I should be redirected to the ""(.*)"" Page")]
+        public void ThenIShouldBeRedirectedToThePage(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}

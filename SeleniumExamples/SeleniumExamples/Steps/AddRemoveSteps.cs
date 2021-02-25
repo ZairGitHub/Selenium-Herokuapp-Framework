a2ff0bb9a4012b520fe25f5e374805ab7901818a
Zair <michaelxzr@gmail.com>
2021-02-25 23:50:53 +0000

    Update AddRemoveSteps.cs

--- SeleniumExamples/SeleniumExamples/Steps/AddRemoveSteps.cs
+++ SeleniumExamples/SeleniumExamples/Steps/AddRemoveSteps.cs
@@ -1,27 +1,39 @@
-﻿using System;
+﻿using NUnit.Framework;
+using SeleniumExamples.Pages;
 using TechTalk.SpecFlow;
 
-namespace SeleniumExamples.Features
+namespace SeleniumExamples.Steps
 {
     [Binding]
     public class AddRemoveSteps
     {
+        private int _result;
+        private WebsitePOM _sut;
+
+        [BeforeScenario]
+        public void BeforeScenario() => _sut = new WebsitePOM(StaticDriver.Type);
+
+        [AfterScenario]
+        public void AfterScenario() => _sut.CloseDriver();
+
         [Given(@"the user is on the Add/Remove page")]
         public void GivenTheUserIsOnTheAddRemovePage()
         {
-            ScenarioContext.Current.Pending();
+            _sut.AddRemovePage.NavigateToPage();
         }
         
         [When(@"the user clicks the add button")]
         public void WhenTheUserClicksTheAddButton()
         {
-            ScenarioContext.Current.Pending();
+            _sut.AddRemovePage.ClickAddButton();
         }
         
         [Then(@"a delete button should be created")]
         public void ThenADeleteButtonShouldBeCreated()
         {
-            ScenarioContext.Current.Pending();
+            var result = _sut.AddRemovePage.CountNumberOfDeleteButtons();
+
+            Assert.That(result, Is.EqualTo(0));
         }
     }
 }

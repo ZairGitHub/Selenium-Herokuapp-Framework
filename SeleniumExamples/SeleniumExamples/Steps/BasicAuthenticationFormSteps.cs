777d6cf0635e2f8d990ceb1f41f55ba5d8dee5b0
Zair <michaelxzr@gmail.com>
2021-02-26 15:54:58 +0000

    Update BasicAuthenticationFormSteps.cs

--- SeleniumExamples/SeleniumExamples/Steps/BasicAuthenticationFormSteps.cs
+++ SeleniumExamples/SeleniumExamples/Steps/BasicAuthenticationFormSteps.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NUnit.Framework;
+using SeleniumExamples.Pages;
 using TechTalk.SpecFlow;
 
 namespace SeleniumExamples.Steps
@@ -6,22 +7,32 @@ namespace SeleniumExamples.Steps
     [Binding]
     public class BasicAuthenticationFormSteps
     {
+        private WebsitePOM _sut;
+
+        [BeforeScenario]
+        public void BeforeScenario() => _sut = new WebsitePOM(StaticDriver.Type);
+
+        [AfterScenario]
+        public void AfterScenario() => _sut.CloseDriver();
+
         [Given(@"the user is on the Basic Authentication Form")]
         public void GivenTheUserIsOnTheBasicAuthenticationForm()
         {
-            ScenarioContext.Current.Pending();
+            _sut.BasicAuthenticationPage.NavigateToAuthentication();
         }
         
         [When(@"the user clicks the cancel button")]
         public void WhenTheUserClicksTheCancelButton()
         {
-            ScenarioContext.Current.Pending();
+            _sut.SharedIAlert.ClickCancelButton();
         }
         
         [Then(@"the page header text should inform the user that they failed to authenticate their credentials")]
         public void ThenThePageHeaderTextShouldInformTheUserThatTheyFailedToAuthenticateTheirCredentials()
         {
-            ScenarioContext.Current.Pending();
+            var result = _sut.SharedHTML.ReadPageHeaderText();
+
+            Assert.That(result, Is.EqualTo("Not authorized"));
         }
     }
 }

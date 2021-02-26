0fed3c19d7ec38de22e2dfbd1f1787c35d67ae62
Zair <michaelxzr@gmail.com>
2021-02-26 18:10:19 +0000

    Create FormAuthenticationSteps.cs

---
+++ SeleniumExamples/SeleniumExamples/Steps/FormAuthenticationSteps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace SeleniumExamples.Steps
+{
+    [Binding]
+    public class FormAuthenticationPageSteps
+    {
+        [Given(@"the user is on the Form Authentication page")]
+        public void GivenTheUserIsOnTheFormAuthenticationPage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"the user clicks the login button")]
+        public void WhenTheUserClicksTheLoginButton()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"an error message containing the following text ""(.*)"" should be displayed")]
+        public void ThenAnErrorMessageContainingTheFollowingTextShouldBeDisplayed(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}

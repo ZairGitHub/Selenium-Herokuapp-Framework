f02a42056d73f3535e4c9cc8b0b7c0de3676cadb
Zair <michaelxzr@gmail.com>
2021-02-26 21:22:43 +0000

    Create JavaScriptAlertsSteps.cs

---
+++ SeleniumExamples/SeleniumExamples/Steps/JavaScriptAlertsSteps.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace SeleniumExamples.Steps
+{
+    [Binding]
+    public class JavaScriptAlertsSteps
+    {
+        [Given(@"the user is on the JavaScriptAlerts page")]
+        public void GivenTheUserIsOnTheJavaScriptAlertsPage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"the user clicks the JSAlert button")]
+        public void WhenTheUserClicksTheJSAlertButton()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"the user clicks the OK button")]
+        public void WhenTheUserClicksTheOKButton()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the page should display the result text ""(.*)"" for the interaction")]
+        public void ThenThePageShouldDisplayTheResultTextForTheInteraction(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}

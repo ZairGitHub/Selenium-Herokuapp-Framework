aa2551b8cc69ebf3e057c8dfa55c7a725d45d680
Zair <michaelxzr@gmail.com>
2021-02-26 18:52:05 +0000

    Create HoversSteps.cs

---
+++ SeleniumExamples/SeleniumExamples/Steps/HoversSteps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace SeleniumExamples.Steps
+{
+    [Binding]
+    public class HoversSteps
+    {
+        [Given(@"the user is on the Hovers page")]
+        public void GivenTheUserIsOnTheHoversPage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"the user hovers over the image (.*)")]
+        public void WhenTheUserHoversOverTheImage(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the user should be able to see additional name information for the image")]
+        public void ThenTheUserShouldBeAbleToSeeAdditionalNameInformationForTheImage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}

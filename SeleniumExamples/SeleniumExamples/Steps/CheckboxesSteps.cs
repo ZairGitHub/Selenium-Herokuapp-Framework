a63de177ff7acd5a850942eb14dc98bfadf8c643
Zair <michaelxzr@gmail.com>
2021-02-26 00:20:02 +0000

    Create CheckboxesSteps.cs

---
+++ SeleniumExamples/SeleniumExamples/Steps/CheckboxesSteps.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace SeleniumExamples.Steps
+{
+    [Binding]
+    public class CheckboxesSteps
+    {
+        [Given(@"the user is on the Checkboxes page")]
+        public void GivenTheUserIsOnTheCheckboxesPage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"the user toggles the checkbox (.*)")]
+        public void GivenTheUserTogglesTheCheckbox(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"the user checks the checkbox (.*) state")]
+        public void WhenTheUserChecksTheCheckboxState(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the checkbox (.*) state should be toggled")]
+        public void ThenTheCheckboxStateShouldBeToggled(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}

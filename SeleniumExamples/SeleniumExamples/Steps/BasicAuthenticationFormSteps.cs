795741a68da3b343351e253642c69f9c3771d4ae
Zair <michaelxzr@gmail.com>
2021-02-26 15:52:32 +0000

    Create BasicAuthenticationFormSteps.cs

---
+++ SeleniumExamples/SeleniumExamples/Steps/BasicAuthenticationFormSteps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace SeleniumExamples.Steps
+{
+    [Binding]
+    public class BasicAuthenticationFormSteps
+    {
+        [Given(@"the user is on the Basic Authentication Form")]
+        public void GivenTheUserIsOnTheBasicAuthenticationForm()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"the user clicks the cancel button")]
+        public void WhenTheUserClicksTheCancelButton()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the page header text should inform the user that they failed to authenticate their credentials")]
+        public void ThenThePageHeaderTextShouldInformTheUserThatTheyFailedToAuthenticateTheirCredentials()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}

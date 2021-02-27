204033cc49c2c4f6e7de1c9cbb3c4548d0b4b3f7
Zair <michaelxzr@gmail.com>
2021-02-27 20:09:18 +0000

    Create ScenarioHooks.cs to handle Hooks

---
+++ SeleniumExamples/SeleniumExamples/Steps/ScenarioHooks.cs
@@ -0,0 +1,30 @@
+﻿using BoDi;
+using TechTalk.SpecFlow;
+using SeleniumExamples.Pages;
+
+namespace SeleniumExamples.Steps
+{
+    [Binding]
+    public class ScenarioHooks
+    {
+        private readonly IObjectContainer _container;
+
+        public ScenarioHooks(IObjectContainer container)
+        {
+            _container = container;
+        }
+
+        [BeforeScenario]
+        public void BeforeScenario()
+        {
+            WebsitePOM sut = new WebsitePOM(StaticDriver.Type);
+            _container.RegisterInstanceAs(sut);
+        }
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+            _container.Resolve<WebsitePOM>().CloseDriver();
+        }
+    }
+}

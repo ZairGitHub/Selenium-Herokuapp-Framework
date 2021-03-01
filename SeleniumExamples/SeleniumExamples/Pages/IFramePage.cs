178e51432b309a77863aa4667fa3c546f0fc0e80
Zair <michaelxzr@gmail.com>
2021-03-01 22:27:35 +0000

    Simplify IFramePage.cs

--- SeleniumExamples/SeleniumExamples/Pages/IFramePage.cs
+++ SeleniumExamples/SeleniumExamples/Pages/IFramePage.cs
@@ -14,15 +14,6 @@ namespace SeleniumExamples.Pages
         private IWebElement TextParagraph =>
             Driver.FindElement(By.CssSelector("p"));
 
-        private IWebElement ButtonSelectAll =>
-            Driver.FindElement(By.CssSelector(".tox-statusbar__path-item"));
-
-        private IWebElement ButtonUndo => Driver.FindElement(By.CssSelector(
-                ".tox-toolbar__group:nth-child(1) > .tox-tbtn:nth-child(1)"));
-
-        private IWebElement ButtonRedo => Driver.FindElement(By.CssSelector(
-                ".tox-toolbar__group:nth-child(1) > .tox-tbtn:nth-child(2)"));
-
         public void EnterText(string text)
         {
             Driver.SwitchTo().Frame(0);
@@ -30,27 +21,5 @@ namespace SeleniumExamples.Pages
         }
 
         public string ReadText() => TextParagraph.Text;
-
-        public void ClickUndoButton()
-        {
-            Driver.SwitchTo().DefaultContent();
-            ButtonUndo.Click();
-            Driver.SwitchTo().Frame(0);
-        }
-
-        public void ClickRedoButton()
-        {
-            Driver.SwitchTo().DefaultContent();
-            ButtonRedo.Click();
-            Driver.SwitchTo().Frame(0);
-        }
-
-        public string ReadHighlightedText()
-        {
-            return ((IJavaScriptExecutor)Driver).ExecuteScript(
-                "return window.getSelection().toString();") as string;
-        }
-
-        public void SelectAllText() => ButtonSelectAll.Click();
     }
 }

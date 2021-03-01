306dfcd7d472ca8cea1bce73de21377516dbbf85
Zair <michaelxzr@gmail.com>
2021-03-01 15:00:24 +0000

    Remove old [Test]s from NestedFramesTests.cs

--- SeleniumExamples/SeleniumExamples/Tests/NestedFramesTests.cs
+++ SeleniumExamples/SeleniumExamples/Tests/NestedFramesTests.cs
@@ -19,48 +19,15 @@ namespace SeleniumExamples.Tests
         [TestCase(NestedFrame.Left, "LEFT")]
         [TestCase(NestedFrame.Middle, "MIDDLE")]
         [TestCase(NestedFrame.Right, "RIGHT")]
-        public void TopFrameNestedFramesText(NestedFrame frame, string expected)
+        public void TopFrameNestedFramesText(NestedFrame nestedFrame, string expected)
         {
             _sut.NestedFramesPage.NavigateToPage();
 
             _sut.NestedFramesPage.SwitchToParentFrame(ParentFrame.Top);
-            _sut.NestedFramesPage.SwitchToNestedFrame(frame);
+            _sut.NestedFramesPage.SwitchToNestedFrame(nestedFrame);
             var result = _sut.SharedHTML.ReadPageBodyText();
 
             Assert.That(result, Is.EqualTo(expected));
-        }
-
-        [Test]
-        public void LeftFrameText()
-        {
-            _sut.NestedFramesPage.NavigateToPage();
-
-            _sut.NestedFramesPage.SwitchToNestedLeftFrame();
-            var result = _sut.SharedHTML.ReadPageBodyText();
-
-            Assert.That(result, Is.EqualTo("LEFT"));
-        }
-
-        [Test]
-        public void MiddleFrameText()
-        {
-            _sut.NestedFramesPage.NavigateToPage();
-
-            _sut.NestedFramesPage.SwitchToNestedMiddleFrame();
-            var result = _sut.SharedHTML.ReadPageBodyText();
-
-            Assert.That(result, Is.EqualTo("MIDDLE"));
-        }
-
-        [Test]
-        public void RightFrameText()
-        {
-            _sut.NestedFramesPage.NavigateToPage();
-
-            _sut.NestedFramesPage.SwitchToNestedRightFrame();
-            var result = _sut.SharedHTML.ReadPageBodyText();
-
-            Assert.That(result, Is.EqualTo("RIGHT"));
         }
     }
 }

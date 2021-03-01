ea06accee6c65e2db6876f9621723bfe68cb7f3f
Zair <michaelxzr@gmail.com>
2021-03-01 12:21:56 +0000

    Add [Test] to FramesTests.cs

--- SeleniumExamples/SeleniumExamples/Tests/FramesTests.cs
+++ SeleniumExamples/SeleniumExamples/Tests/FramesTests.cs
@@ -1,10 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using NUnit.Framework;
+using SeleniumExamples.Pages;
 
 namespace SeleniumExamples.Tests
 {
-    class FramesTests
+    [TestFixture]
+    public class FramesTests
     {
+        private PageFactory _sut;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp() => _sut = new PageFactory(StaticDriver.Type);
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown() => _sut.CloseDriver();
+
+        [Test]
+        public void NestedFramesLink_RedirectsToNestedFramesPage()
+        {
+            _sut.FramesPage.NavigateToPage();
+
+            _sut.FramesPage.ClickNestedFramesLink();
+            var result = _sut.Driver.Url;
+
+            Assert.That(result,
+                Is.EqualTo(ConfigReader.Index + ConfigReader.NestedFrames));
+        }
     }
 }

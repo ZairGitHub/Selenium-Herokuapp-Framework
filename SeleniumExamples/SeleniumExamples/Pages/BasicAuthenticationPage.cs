916faa5a418714b29d5a7af3089ba28702d7a0e6
Zair <michaelxzr@gmail.com>
2021-03-01 00:12:05 +0000

    Update BasicAuthenticationPage.cs

--- SeleniumExamples/SeleniumExamples/Pages/BasicAuthenticationPage.cs
+++ SeleniumExamples/SeleniumExamples/Pages/BasicAuthenticationPage.cs
@@ -2,27 +2,22 @@
 
 namespace SeleniumExamples.Pages
 {
-    public sealed class BasicAuthenticationPage : WebPage, IAlertNavigation
+    public sealed class BasicAuthenticationPage : WebPage, IAlertNavigation, IPageNavigation
     {
         private const string _validUsername = "admin";
         private const string _validPassword = "admin";
 
         public BasicAuthenticationPage(IWebDriver driver) : base(driver) { }
 
-        public void NavigateToInvalidAuthentication()
+        public void NavigateToPage()
         {
             NavigateToURL(ConfigReader.Index + ConfigReader.BasicAuthentication);
         }
 
-        public void NavigateToValidAuthentication()
+        public void NavigatePastAuthentication()
         {    
-            NavigateToURL(
-                $"http://{_validUsername}:{_validPassword}@" +
+            NavigateToURL($"http://{_validUsername}:{_validPassword}@" +
                 ConfigReader.Base + ConfigReader.BasicAuthentication);
         }
-
-        public string ValidUsername => _validUsername;
-
-        public string ValidPassword => _validPassword;
     }
 }

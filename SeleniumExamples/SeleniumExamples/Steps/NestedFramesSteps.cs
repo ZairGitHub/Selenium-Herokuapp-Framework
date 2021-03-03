20a8c683a40e7225c08efae362d4dba0f9a569d2
Zair <michaelxzr@gmail.com>
2021-03-03 15:39:21 +0000

    Create NestedFramesSteps.cs

---
+++ SeleniumExamples/SeleniumExamples/Steps/NestedFramesSteps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace SeleniumExamples.Steps
+{
+    [Binding]
+    public class NestedFramesSteps
+    {
+        [Given(@"the user is on the Nested Frames page")]
+        public void GivenTheUserIsOnTheNestedFramesPage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"the user switches to the frame Frame\.Left")]
+        public void WhenTheUserSwitchesToTheFrameFrame_Left()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the body of the frame should display the correct text LEFT")]
+        public void ThenTheBodyOfTheFrameShouldDisplayTheCorrectTextLEFT()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}

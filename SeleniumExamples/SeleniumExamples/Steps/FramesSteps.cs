3726e6b483f7f8b103bd399a0a26016633b07f90
Zair <michaelxzr@gmail.com>
2021-03-03 15:19:21 +0000

    Create FramesSteps.cs

---
+++ SeleniumExamples/SeleniumExamples/Steps/FramesSteps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace SeleniumExamples.Steps
+{
+    [Binding]
+    public class FramesSteps
+    {
+        [Given(@"the user is on the Frames page")]
+        public void GivenTheUserIsOnTheFramesPage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"the user clicks the NestedFrames link")]
+        public void WhenTheUserClicksTheNestedFramesLink()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"the user clicks the iFrames link")]
+        public void WhenTheUserClicksTheIFramesLink()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}

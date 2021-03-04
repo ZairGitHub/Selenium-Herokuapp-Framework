ba4f2397f2a27c5dd6e9d715e62d0885c4ca41e9
Zair <michaelxzr@gmail.com>
2021-03-04 15:15:32 +0000

    Create DropdownSteps.cs

---
+++ SeleniumExamples/SeleniumExamples/Steps/DropdownSteps.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace SeleniumExamples.Steps
+{
+    [Binding]
+    public class DropdownSteps
+    {
+        [Given(@"the user is on the Dropdown page")]
+        public void GivenTheUserIsOnTheDropdownPage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the dropdown list should not display a selected option")]
+        public void ThenTheDropdownListShouldNotDisplayASelectedOption()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}

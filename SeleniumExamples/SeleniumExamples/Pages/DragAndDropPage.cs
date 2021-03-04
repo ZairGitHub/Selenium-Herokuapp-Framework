384d88bd4c5bd176138a86a72c2f1a1499cb0074
Zair <michaelxzr@gmail.com>
2021-03-04 11:49:37 +0000

    Update DragAndDropPage.cs

--- SeleniumExamples/SeleniumExamples/Pages/DragAndDropPage.cs
+++ SeleniumExamples/SeleniumExamples/Pages/DragAndDropPage.cs
@@ -16,19 +16,23 @@ namespace SeleniumExamples.Pages
         
         private IWebElement ColumnB => Driver.FindElement(By.Id("column-b"));
 
-        private int ColumnAPosition => ColumnA.Location.X;
+        private IWebElement ColumnAHeader =>
+            Driver.FindElement(By.CssSelector("#column-a > header"));
 
-        private int ColumnBPosition => ColumnB.Location.X;
+        private IWebElement ColumnBHeader =>
+            Driver.FindElement(By.CssSelector("#column-b > header"));
 
         public void SwapPositions()
         {
-            new Actions(Driver).DragAndDrop(ColumnA, ColumnB)
+            new Actions(Driver)
+                .DragAndDrop(ColumnA, ColumnB)
                 .Perform();
         }
         
         public bool HaveColumnPositionsBeenSwapped()
         {
-            return ColumnAPosition > ColumnBPosition;
+            return ColumnAHeader.Text != "A";
+            //32 //247
         }
     }
 }

06a61be04881ea9a02a314b53f1693715a956bd0
Zair <michaelxzr@gmail.com>
2021-03-04 12:30:48 +0000

    Add [Test] to DropdownTests.cs

--- SeleniumExamples/SeleniumExamples/Tests/DropdownTests.cs
+++ SeleniumExamples/SeleniumExamples/Tests/DropdownTests.cs
@@ -1,10 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using NUnit.Framework;
+using SeleniumExamples.Pages;
 
 namespace SeleniumExamples.Tests
 {
-    class DropdownTests
+    [TestFixture]
+    public class DropdownTests
     {
+        private PageFactory _sut;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp() => _sut = new PageFactory(StaticDriver.Type);
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown() => _sut.CloseDriver();
+
+        [Test]
+        public void Option1()
+        {
+            _sut.DropdownPage.NavigateToPage();
+
+            _sut.DropdownPage.ClickOption1();
+            var result = _sut.DropdownPage.ReadDropdownText();
+
+            Assert.That(result, Is.EqualTo("Option 1"));
+        }
     }
 }

59d5e5451bf56949650f51af2f5c9b8c26a8793b
Zair <michaelxzr@gmail.com>
2021-03-04 10:28:02 +0000

    Add failing [Test] to DragAndDropTests.cs

--- SeleniumExamples/SeleniumExamples/Tests/DragAndDropTests.cs
+++ SeleniumExamples/SeleniumExamples/Tests/DragAndDropTests.cs
@@ -1,10 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using NUnit.Framework;
+using SeleniumExamples.Pages;
 
 namespace SeleniumExamples.Tests
 {
-    class DragAndDropTests
+    [TestFixture]
+    public class DragAndDropTests
     {
+        private PageFactory _sut;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp() => _sut = new PageFactory(StaticDriver.Type);
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown() => _sut.CloseDriver();
+
+        [Test]
+        public void SwapPositions_SwapsThePositionsOfTwoElements()
+        {
+            _sut.DragAndDropPage.NavigateToPage();
+
+            _sut.DragAndDropPage.SwapPositions();
+            var result = _sut.DragAndDropPage.HaveColumnPositionsBeenSwapped();
+
+            Assert.That(result, Is.True);
+        }
     }
 }

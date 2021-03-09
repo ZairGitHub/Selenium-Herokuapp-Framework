23c3f99f752af2670033b5a3b700a697882e9218
Zair <michaelxzr@gmail.com>
2021-03-09 22:43:27 +0000

    Create DragAndDropSteps.cs

---
+++ SeleniumExamples/SeleniumExamples/Steps/DragAndDropSteps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace SeleniumExamples.Steps
+{
+    [Binding]
+    public class DragAndDropSteps
+    {
+        [Given(@"the user is on the Drag and Drop Page")]
+        public void GivenTheUserIsOnTheDragAndDropPage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I drag a draggable element and drop it on top of another draggable element")]
+        public void GivenIDragADraggableElementAndDropItOnTopOfAnotherDraggableElement()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the contents of the elements should be swapped ""(.*)""")]
+        public void ThenTheContentsOfTheElementsShouldBeSwapped(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}

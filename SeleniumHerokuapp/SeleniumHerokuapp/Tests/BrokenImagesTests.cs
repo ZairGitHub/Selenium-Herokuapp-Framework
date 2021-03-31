a6c37927f133891f50f885a6f0d6f326205a1cd3
Zair <michaelxzr@gmail.com>
2021-03-31 21:01:03 +0100

    Create BrokenImagesTests.cs

---
+++ SeleniumHerokuapp/SeleniumHerokuapp/Tests/BrokenImagesTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using SeleniumHerokuapp.Pages;
+
+namespace SeleniumHerokuapp.Tests
+{
+    [TestFixture]
+    public class BrokenImagesTests
+    {
+        private PageFactory _sut;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp() => _sut = new PageFactory(StaticDriver.Type);
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown() => _sut.CloseDriver();
+    }
+}

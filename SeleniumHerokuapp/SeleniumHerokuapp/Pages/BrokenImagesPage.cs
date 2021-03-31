f72970f596b0b4fdb448c523723eda6be0882819
Zair <michaelxzr@gmail.com>
2021-03-31 20:48:12 +0100

    Create BrokenImagesPage.cs

---
+++ SeleniumHerokuapp/SeleniumHerokuapp/Pages/BrokenImagesPage.cs
@@ -0,0 +1,14 @@
+﻿using OpenQA.Selenium;
+
+namespace SeleniumHerokuapp.Pages
+{
+    public sealed class BrokenImagesPage : WebPage, IPageNavigation
+    {
+        public BrokenImagesPage(IWebDriver driver) : base(driver) { }
+
+        public void NavigateToPage()
+        {
+            NavigateToURL(ConfigReader.Index + ConfigReader.BrokenImages);
+        }
+    }
+}

0ffa401856f9862feb45f3e2b2ba0f8a373d0973
Zair <michaelxzr@gmail.com>
2021-03-31 23:42:36 +0100

    Update InputsTests.cs

--- SeleniumHerokuapp/SeleniumHerokuapp/Tests/InputsTests.cs
+++ SeleniumHerokuapp/SeleniumHerokuapp/Tests/InputsTests.cs
@@ -3,6 +3,8 @@ using SeleniumHerokuapp.Pages;
 
 namespace SeleniumHerokuapp.Tests
 {
+    // Tests for increment/decrement doubles
+    // Tests for strings
     [TestFixture]
     public class InputsTests
     {
@@ -18,33 +20,34 @@ namespace SeleniumHerokuapp.Tests
         public void InputNumber()
         {
             _sut.InputsPage.NavigateToPage();
+            int input = 9;
 
-            _sut.InputsPage.EnterValue(9);
+            _sut.InputsPage.EnterValue(input);
             var result =  _sut.InputsPage.ReadValue();
 
-            Assert.That(result, Is.EqualTo(9.ToString()));
+            Assert.That(result, Is.EqualTo(input));
         }
 
         [Test]
-        public void Increment()
+        public void IncrementValue_IncrementsTheCurrentInput()
         {
             _sut.InputsPage.NavigateToPage();
 
-            _sut.InputsPage.IncreaseValue();
+            _sut.InputsPage.IncrementValue();
             var result = _sut.InputsPage.ReadValue();
 
-            Assert.That(result, Is.EqualTo("1"));
+            Assert.That(result, Is.EqualTo(1));
         }
 
         [Test]
-        public void Decrement()
+        public void DecrementValue_DecrementsTheCurrentInput()
         {
             _sut.InputsPage.NavigateToPage();
 
-            _sut.InputsPage.DecreaseValue();
+            _sut.InputsPage.DecrementValue();
             var result = _sut.InputsPage.ReadValue();
 
-            Assert.That(result, Is.EqualTo("-1"));
+            Assert.That(result, Is.EqualTo(-1));
         }
     }
 }

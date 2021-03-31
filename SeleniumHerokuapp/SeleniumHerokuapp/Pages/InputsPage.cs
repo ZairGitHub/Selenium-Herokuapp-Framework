d747da4c40d0e2c24e7787372dd1ce49b026f46f
Zair <michaelxzr@gmail.com>
2021-03-31 22:00:33 +0100

    Create InputsPage.cs

---
+++ SeleniumHerokuapp/SeleniumHerokuapp/Pages/InputsPage.cs
@@ -0,0 +1,14 @@
+﻿using OpenQA.Selenium;
+
+namespace SeleniumHerokuapp.Pages
+{
+    public sealed class InputsPage : WebPage, IPageNavigation
+    {
+        public InputsPage(IWebDriver driver) : base(driver) { }
+
+        public void NavigateToPage()
+        {
+            NavigateToURL(ConfigReader.Index + ConfigReader.Inputs);
+        }
+    }
+}

085447f7a1e4d9fdc3ea765938ca5ccbfc0576fe
Zair <michaelxzr@gmail.com>
2021-03-31 21:16:01 +0100

    Create BrokenImagesSteps.cs

---
+++ SeleniumHerokuapp/SeleniumHerokuapp/Steps/BrokenImagesSteps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace SeleniumHerokuapp.Steps
+{
+    [Binding]
+    public class BrokenImagesSteps
+    {
+        [Given(@"the user is on the Broken Images page")]
+        public void GivenTheUserIsOnTheBrokenImagesPage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"the user checks if an image is broken")]
+        public void WhenTheUserChecksIfAnImageIsBroken()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the condition of the image should reflect this information")]
+        public void ThenTheConditionOfTheImageShouldReflectThisInformation()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}

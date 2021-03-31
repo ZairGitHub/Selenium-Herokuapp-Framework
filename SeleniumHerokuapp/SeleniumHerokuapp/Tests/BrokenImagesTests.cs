4400fca55760ad01a578a062c950e8f8b7517e92
Zair <michaelxzr@gmail.com>
2021-03-31 21:05:04 +0100

    Add [TestCase]s to BrokenImagesTests.cs

--- SeleniumHerokuapp/SeleniumHerokuapp/Tests/BrokenImagesTests.cs
+++ SeleniumHerokuapp/SeleniumHerokuapp/Tests/BrokenImagesTests.cs
@@ -13,5 +13,18 @@ namespace SeleniumHerokuapp.Tests
 
         [OneTimeTearDown]
         public void OneTimeTearDown() => _sut.CloseDriver();
+
+        [TestCase(1, true)]
+        [TestCase(2, true)]
+        [TestCase(3, false)]
+        public void IsImageBroken_ReturnsTrueIfImageIsBroken(
+            int id, bool expectedCondition)
+        {
+            _sut.BrokenImagesPage.NavigateToPage();
+
+            var result = _sut.BrokenImagesPage.IsImageBroken(id);
+
+            Assert.That(result, Is.EqualTo(expectedCondition));
+        }
     }
 }

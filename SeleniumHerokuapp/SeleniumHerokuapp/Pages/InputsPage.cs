d8f6837d62b65cde39543822bd66d3024ce37283
Zair <michaelxzr@gmail.com>
2021-03-31 22:14:48 +0100

    Add property and methods to InputsPage.cs

--- SeleniumHerokuapp/SeleniumHerokuapp/Pages/InputsPage.cs
+++ SeleniumHerokuapp/SeleniumHerokuapp/Pages/InputsPage.cs
@@ -10,5 +10,28 @@ namespace SeleniumHerokuapp.Pages
         {
             NavigateToURL(ConfigReader.Index + ConfigReader.Inputs);
         }
+
+        private IWebElement FieldInput =>
+            Driver.FindElement(By.CssSelector("input"));
+
+        public void EnterValue(int value)
+        {
+            FieldInput.SendKeys(value.ToString());
+        }
+
+        public void EnterValue(string value)
+        {
+            FieldInput.SendKeys(value);
+        }
+
+        public void IncreaseValue()
+        {
+            FieldInput.Click();
+        }
+
+        public void DecreaseValue()
+        {
+            FieldInput.Click();
+        }
     }
 }

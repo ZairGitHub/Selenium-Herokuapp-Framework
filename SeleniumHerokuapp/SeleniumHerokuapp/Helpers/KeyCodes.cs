702ac88eeb2d21fef2431f11f98496fa6fc151f3
Zair <michaelxzr@gmail.com>
2021-04-01 20:40:59 +0100

    Create KeyCodes.cs

---
+++ SeleniumHerokuapp/SeleniumHerokuapp/Helpers/KeyCodes.cs
@@ -0,0 +1,11 @@
+﻿namespace SeleniumHerokuapp.Helpers
+{
+    public static class KeyCodes
+    {
+        public const string BackSpace = "\uE003";
+
+        public const string Escape = "\uE00C";
+
+        public const string Tab = "\uE004";
+    }
+}

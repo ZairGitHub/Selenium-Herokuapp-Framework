3b8c55f9d7e5d6c0336a79c5d9f833aab450dd32
Zair <michaelxzr@gmail.com>
2021-04-01 19:13:19 +0100

    Create KeyPressesPage.cs

---
+++ SeleniumHerokuapp/SeleniumHerokuapp/Pages/KeyPressesPage.cs
@@ -0,0 +1,14 @@
+﻿using OpenQA.Selenium;
+
+namespace SeleniumHerokuapp.Pages
+{
+    public sealed class KeyPressesPage : WebPage, IPageNavigation
+    {
+        public KeyPressesPage(IWebDriver driver) : base(driver) { }
+
+        public void NavigateToPage()
+        {
+            NavigateToURL(ConfigReader.Index + ConfigReader.KeyPresses);
+        }
+    }
+}

e6303906ff48425516be2983a0825253e56de87a
Zair <michaelxzr@gmail.com>
2021-04-02 12:29:39 +0100

    Add properties and methods to EntryAdPage.cs

--- SeleniumHerokuapp/SeleniumHerokuapp/Pages/EntryAdPage.cs
+++ SeleniumHerokuapp/SeleniumHerokuapp/Pages/EntryAdPage.cs
@@ -10,5 +10,20 @@ namespace SeleniumHerokuapp.Pages
         {
             NavigateToURL(ConfigReader.Index + ConfigReader.EntryAd);
         }
+
+        private IWebElement LinkRestartAd =>
+            Driver.FindElement(By.Id("restart-ad"));
+
+        private IWebElement ModalTitle =>
+            Driver.FindElement(By.CssSelector(".modal-title > h3"));
+
+        private IWebElement ModalFooter =>
+            Driver.FindElement(By.CssSelector(".modal-footer > p"));
+
+        public string ReadModalWindowTitle() => ModalTitle.Text;
+
+        public void ReenableModalWindow() => LinkRestartAd.Click();
+
+        public void CloseModalWindow() => ModalFooter.Click();
     }
 }

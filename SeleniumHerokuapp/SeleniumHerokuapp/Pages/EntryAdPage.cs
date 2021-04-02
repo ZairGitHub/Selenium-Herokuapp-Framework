3d2b256a7d761ace465de8caadaa65819b80a9cb
Zair <michaelxzr@gmail.com>
2021-04-02 12:22:22 +0100

    Create EntryAdPage.cs

---
+++ SeleniumHerokuapp/SeleniumHerokuapp/Pages/EntryAdPage.cs
@@ -0,0 +1,14 @@
+﻿using OpenQA.Selenium;
+
+namespace SeleniumHerokuapp.Pages
+{
+    public sealed class EntryAdPage : WebPage, IPageNavigation
+    {
+        public EntryAdPage(IWebDriver driver) :base(driver) { }
+
+        public void NavigateToPage()
+        {
+            NavigateToURL(ConfigReader.Index + ConfigReader.EntryAd);
+        }
+    }
+}

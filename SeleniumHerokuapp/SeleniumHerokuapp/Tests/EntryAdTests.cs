e3dbb0d852a041565937a879d5f014a7247d973f
Zair <michaelxzr@gmail.com>
2021-04-02 12:31:27 +0100

    Create EntryAdTests.cs

---
+++ SeleniumHerokuapp/SeleniumHerokuapp/Tests/EntryAdTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using SeleniumHerokuapp.Pages;
+
+namespace SeleniumHerokuapp.Tests
+{
+    [TestFixture]
+    public class EntryAdTests
+    {
+        private PageFactory _sut;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp() => _sut = new PageFactory(StaticDriver.Type);
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown() => _sut.CloseDriver();
+    }
+}
